fea98de19362eebd67d543457b5cd220de144cea
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-18 12:45:24 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -48,7 +48,7 @@ const News = (props)=>{
     // }
 
     const fetchMoreData = async () => {   
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
         setPage(page+1)
         let data = await fetch(url);
         let parsedData = await data.json()
