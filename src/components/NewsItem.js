439c83c008c3d77320da43ef4d46f3033bf1393d
Vaibhav Jajodia <71918594+vaibhavj-11@users.noreply.github.com>
2023-08-18 12:45:44 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,9 +7,9 @@ const NewsItem = (props) => {
         <div className={`card bg-${props.mode}`}>
             <img src={!imageUrl?"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/11787/production/_124395517_bbcbreakingnewsgraphic.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>{title}</h5>
+                <a href={newsUrl} target="_blank" className="text-underline-hover" ><h5 className="card-title" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>{title}</h5></a>
                 <p className="card-text" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>{description}</p>
-                <p className="card-text"><small className="text-muted" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+                <p className="card-text"><small className="text" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                 <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
